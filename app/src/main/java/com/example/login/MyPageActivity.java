937ed281077bb9e6378b681568fd5d4c9a339b1e
bmh1211 <64722559+bmh1211@users.noreply.github.com>
2020-11-16 17:57:18 +0900

    201116 18:00

--- app/src/main/java/com/example/login/MyPageActivity.java
+++ app/src/main/java/com/example/login/MyPageActivity.java
@@ -2,17 +2,27 @@ package com.example.login;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MyPageActivity extends AppCompatActivity {
-//    ActionBar ab = getSupportActionBar();
+    private Toolbar tb_myPage;
+    private ActionBar ab_myPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
 
-//        ab.setTitle("ActionBar Title by setTitle()");
+        tb_myPage = (Toolbar)findViewById(R.id.tb_myPage);
+        //여기부터!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        setSupportActionBar(tb_myPage);
+        ab_myPage=getSupportActionBar();
+//        ab_myPage.setDisplayShowCustomEnabled(true);
+//        ab_myPage.setDisplayShowTitleEnabled(false); //기본 제목을 없애줌
+//        ab_myPage.setDisplayHomeAsUpEnabled(true); //자동으로 뒤로가기 버튼을 만들어줌
     }
 }
+// reference : https://game-happy-world.tistory.com/11
