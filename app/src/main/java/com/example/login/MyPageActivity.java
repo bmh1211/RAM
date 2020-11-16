f33f00358eb4eaff25e38ccae45f72cdf78728b0
bmh1211 <64722559+bmh1211@users.noreply.github.com>
2020-11-16 10:23:04 +0900

    201116 10:20

--- app/src/main/java/com/example/login/MyPageActivity.java
+++ app/src/main/java/com/example/login/MyPageActivity.java
@@ -1,14 +1,18 @@
 package com.example.login;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class MyPageActivity extends AppCompatActivity {
+    ActionBar ab = getSupportActionBar();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
+
+        ab.setTitle("ActionBar Title by setTitle()");
     }
 }
