34046ae5beef7bb09cc70fd813f18166f56e8a27
bmh1211 <bmh1211@gmail.com>
2020-11-25 17:16:13 +0900

    move fragment to fragment - mino

--- Android/app/src/main/java/com/example/login/Fragment1.java
+++ Android/app/src/main/java/com/example/login/Fragment1.java
@@ -9,13 +9,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
-import android.widget.ScrollView;
 import android.widget.Toast;
 
 public class Fragment1 extends Fragment {
     private ListView lv_board;
     static final String[] LIST_MENU={"LIST_1","LIST_2","LIST_3","LIST_4","LIST_5","LIST_6","LIST_7","LIST_8","LIST_9"};
+    private Button btn_write;
+    Fragment postingFragment;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,9 +25,10 @@ public class Fragment1 extends Fragment {
         View view = inflater.inflate(R.layout.fragment_1, container, false);
 
         lv_board = (ListView)view.findViewById(R.id.lv_board);
+        postingFragment = new PostingFragment();
+        btn_write=(Button)view.findViewById(R.id.btn_write);
 
         ArrayAdapter board_adapter = new ArrayAdapter(getActivity().getApplicationContext(),android.R.layout.simple_list_item_1,LIST_MENU);
-
         lv_board.setAdapter(board_adapter);
 
         lv_board.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -33,6 +36,14 @@ public class Fragment1 extends Fragment {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String strText = (String) parent.getItemAtPosition(position) ;
                 Toast.makeText(getActivity().getApplicationContext(), strText+"번째 아이템, id : "+id, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO:게시물 작성 버튼눌렀을 때 게시물 작성 창 띄우기
+                ((MainPageActivity)getActivity()).getSupportFragmentManager().beginTransaction().replace(R.id.container,postingFragment).commit();
             }
         });
 
--- Android/app/src/main/java/com/example/login/PostingFragment.java
+++ Android/app/src/main/java/com/example/login/PostingFragment.java
@@ -7,12 +7,39 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class PostingFragment extends Fragment {
+    private Button btn_cancel;
+    private Button btn_apply;
+    private ListView lv_posting;
+    static final String[] LIST_MENU={"1","2","3","4","5","6","7","8","9"};
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_posting, container, false);
+
+        btn_cancel=(Button)view.findViewById(R.id.btn_cancel);
+        btn_apply=(Button)view.findViewById(R.id.btn_apply);
+
+        lv_posting=(ListView)view.findViewById(R.id.lv_posting);
+        ArrayAdapter board_adapter = new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_1,LIST_MENU);
+        lv_posting.setAdapter(board_adapter);
+
+        lv_posting.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String strText = (String) parent.getItemAtPosition(position) ;
+                Toast.makeText(getActivity().getApplicationContext(), strText+"번째 아이템, id : "+id, Toast.LENGTH_SHORT).show();
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_posting, container, false);
+        return view;
     }
 }
